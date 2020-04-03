728dd122bd3e3bf6e6bdcadd35439cd90c7e9b2b
Vincent Josephs <josephsv228@gmail.com>
2020-04-03 19:37:48 -0400

    added the basics of User class

--- source/User.java
+++ source/User.java
@@ -1,6 +1,42 @@
+import java.util.Date;
 package source;
-
+import java.util.ArrayList; 
 public class User {
-
-
+	
+	private String firstName;
+	private String lastName;
+	private String username;
+	private Date birthday; 
+	private String paymentInfo;
+	private String password;
+	private int RewardPoints;
+	private ArrayList<Ticket> pastTransactions = new ArrayList<Ticket>();
+	
+	public User(String firstName, String lastName, Date birthday, String password) { 
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.birthday = birthday;
+		this.password = password;
+	}
+	
+	public purchaseTicket(String name, String address, Date showTime, String paymentInfo) {
+		// fill out later
+	
+	}
+	
+	public giveRating(double rating, String comment) {
+		// fill out later
+	}
+	
+	public ShoppingCart getCurrentCart() {
+		// fill out later
+	}
+	
+	public setPaymentInfo() {
+		// fill out later
+	}
+	
+	public completePurchase(ShoppingCart cart) {
+		// fill out later
+	}
 }
