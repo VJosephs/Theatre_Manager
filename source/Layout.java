3290e2cb5d84910636aa15fca76f87fa6f2e9df2
Vincent Josephs <josephsv228@gmail.com>
2020-04-04 02:05:21 -0400

    fix layout

--- source/Layout.java
+++ source/Layout.java
@@ -11,30 +11,21 @@ public class Layout {
     }
 
     public void printLayout() {
-<<<<<<< HEAD
-    	for (int i = 0; i < seats.length; i++)
-		{
-			for (int j = 0; (seats[i] != null && j < seats[i].length); j++)
-				System.out.print(seats[i][j].getType() + " ");
 
-			System.out.println();
-		}
-=======
         for (int i = 0; i < seats.length + 1; ++i) {
             for (int j = 0; j < seats[0].length + 1; ++j) {
                 if (i == 0 && j == 0)
                     System.out.print("  ");
                 else if(i == 0)
                     System.out.print((char)((j-1)+65) + " ");
-                else if(j == 0) {
+                else if(j == 0) 
                     System.out.print(i + " ");
-                }
                 else
                     System.out.print(seats[i-1][j-1] + " ");
             }
             System.out.println();
         }
->>>>>>> 96b28faca3fcfa06631c7d0d826d35431310f646
+
     }
     public void makeLayout() {
         //TODO add method body
