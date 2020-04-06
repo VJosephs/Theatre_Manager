8cab89201d0efa0f80c39c70387ab9213182d00b
Andrew Merrill <realcoldfate@gmail.com>
2020-04-06 01:51:07 -0400

    First Draft of Searcher class

--- source/Searcher.java
+++ source/Searcher.java
@@ -1,36 +1,102 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Objects;
 
 public class Searcher {
 
     private ArrayList<Theatre> theatres;
+    private ArrayList<Show> shows;
 
-    public Searcher(ArrayList<Theatre> theatres) {
+    public Searcher(ArrayList<Theatre> theatres, ArrayList<Show> shows) {
         setTheatres(theatres);
+        setShows(shows);
     }
 
-    public ArrayList<Show> searchRatings() {
-        //TODO add method body
-        return null;
+    public ArrayList<Show> searchShowRatings() {
+        shows.sort(Show::compareTo);
+        return shows;
     }
 
-    public ArrayList<Show> searchGenre() {
-        //TODO add method body
-        return null;
+    public ArrayList<Theatre> searchTheatreRatings() {
+        theatres.sort(Theatre::compareTo);
+        return theatres;
     }
 
-    public ArrayList<Show> searchNames() {
-        //TODO add method body
-        return null;
+    public ArrayList<Show> searchGenre(String genre) {
+        ArrayList<Show> retShows = new ArrayList<>();
+        for (Show show : shows) {
+            if (show.getGenre().contains(genre))
+                retShows.add(show);
+        }
+        return retShows;
     }
 
-    public ArrayList<Show> searchCast() {
-        //TODO add method body
-        return null;
+    public ArrayList<Show> searchShowNames(String name) {
+        ArrayList<Show> retShows = new ArrayList<>();
+        for (Show show : shows) {
+            if (show.getName().contains(name))
+                retShows.add(show);
+        }
+        return retShows;
     }
 
+    public ArrayList<Theatre> searchTheatreNames(String name) {
+        ArrayList<Theatre> retTheatres = new ArrayList<>();
+        for (Theatre theatre : theatres) {
+            if (theatre.getName().contains(name))
+                retTheatres.add(theatre);
+        }
+        return retTheatres;
+    }
+
+    public ArrayList<Show> searchCast(String castMember) {
+        ArrayList<Show> retShows = new ArrayList<>();
+        for (Show show : shows) {
+            if (show.getCast().contains(castMember)) {
+                retShows.add(show);
+            }
+        }
+        return retShows;
+    }
+
+    // TODO probably fix this
+    public ArrayList<Show> searchType(String type) {
+        ArrayList<Show> retShows = new ArrayList<>();
+        for (Show show : shows) {
+            if (type.equalsIgnoreCase("Movie")) {
+                if (show.getClass() == Movie.class)
+                    retShows.add(show);
+            }
+            else if (type.equalsIgnoreCase("Play")) {
+                if(show.getClass() == Play.class)
+                    retShows.add(show);
+            }
+            else if (type.equalsIgnoreCase("Concert")) {
+                if (show.getClass() == Concert.class)
+                    retShows.add(show);
+            }
+            else {
+                return retShows;
+            }
+        }
+        return retShows;
+    }
+
+    public void reverseList(ArrayList<Object> list) {
+        Collections.reverse(list);
+    }
+
+    public void printList(ArrayList<Object> list) {
+        for (Object o : list) {
+            System.out.println(o.toString());
+        }
+    }
 
     public void setTheatres(ArrayList<Theatre> theatres) {
         this.theatres = Objects.requireNonNullElseGet(theatres, ArrayList::new);
+    }
+
+    public void setShows(ArrayList<Show> shows) {
+        this.shows = Objects.requireNonNullElseGet(shows, ArrayList::new);
     }
 }
