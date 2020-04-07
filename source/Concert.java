1ccdf80420caa9ff52a6fc9f96b542b64c700734
mcHarold <47832638+mcHarold@users.noreply.github.com>
2020-04-07 09:55:07 -0400

    Update Concert.java

--- source/Concert.java
+++ source/Concert.java
@@ -12,6 +12,8 @@ public class Concert extends Show {
      * @param showTime    The time that the concert is being played
      * @param location    The theater that the concert is at.
      * @param price       The amount a ticket for the concert costs
+     * @return n/a   its a constructor
+     * @exception  n/a
      */
     public Concert(String name, String description, String genre, String ageRating, Date showTime, String location, double price) {
         super(name, description, genre, ageRating, showTime, location, price);
@@ -19,8 +21,9 @@ public class Concert extends Show {
 
     /**
      * Returns the age required to go to the concert based on its rating
-     *
-     * @return The minimum age required to go to the concert
+     * @param         none
+     * @return int    the minimum age required to go to the concert
+     * @exception     n/a
      */
     public int getAgeRating() {
         if (ageRating.equals("18+"))
