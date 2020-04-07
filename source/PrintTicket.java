d12b9bc0ae3918fd89c920f7354973e145d2a12b
mcHarold <47832638+mcHarold@users.noreply.github.com>
2020-04-07 03:12:59 -0400

    Update PrintTicket.java

--- source/PrintTicket.java
+++ source/PrintTicket.java
@@ -1,36 +1,38 @@
-import java.io.FileWriter;
+
 import java.util.ArrayList;
-import java.io.BufferedReader;
-import java.io.FileReader;
+
+import java.io.FileWriter;
 import java.io.IOException;
-
-public class PrintTicket extends ShoppingCart<Ticket>{
+ 
+import org.json.JSONArray;
+import org.json.JSONObject;
     
-    public String PrintingTicket() {
-   
-    
-    	 try {
-             file = new FileWriter("JSONReader.txt");
-             JSONReaderLog("Thank you for your purchase to see"+getShow() );
-             JSONReaderLog("The show you have paid to see will be at "+getLocation() );
-             JSONReaderLog("And finally you paid" + getPrice() + "for your ticket!");
-             JSONReaderLog("Thanks again for purchasing at THE_247_PROJECT. Have a nice day and call (***)-***-**** if you have any issues!");
-         } 
-    	 catch (IOException e)
-    	 {
-             e.printStackTrace();
-         }
-    	 finally
-    	 {
-    		 try 
-    		 {
-    			file.flush();
-    			file.close();
-    		 }
-    		 catch (IOException e)
-    		 {
-    			 e.printStackTrace();
-    		 }
-    	 }
-
+public class PrintTicket
+{
+    public static void main( String[] args )
+    {
+        //First Employee
+        JSONObject ticketDetails = new JSONObject();
+        ticketDetails.put("firstName", "Lokesh");
+        ticketDetails.put("lastName", "Gupta");
+        ticketDetails.put("website", "howtodoinjava.com");
+         
+        JSONObject ticketObject = new JSONObject(); 
+        ticketObject.put("tickets", ticketDetails);
+         
+         
+        //Add employees to list
+        JSONArray ticketList = new JSONArray();
+        ticketList.add(ticketObject);       
+        //Write JSON file
+        try (FileWriter file = new FileWriter("tickets.json")) {
+ 
+            file.write(ticketList.toString());
+            file.flush();
+ 
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
+
