be8272d35901217be0c7afd368b14f36c1907074
mcHarold <47832638+mcHarold@users.noreply.github.com>
2020-04-07 09:43:03 -0400

    Update SeniorTicket.java

--- source/SeniorTicket.java
+++ source/SeniorTicket.java
@@ -7,6 +7,8 @@ public class SeniorTicket extends Ticket {
      * Creates a senior ticket for the show
      *
      * @param show The show that the ticket is for
+     * @return n/a     its a constructor
+     * @exception  n/a
      */
     public SeniorTicket(String show, String location, double price) {
         super(show, location, price);
