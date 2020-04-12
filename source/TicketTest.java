93e9a8d2bdfe192a9924e137c9959b3856b03ab0
Vincent Josephs <josephsv228@gmail.com>
2020-04-11 23:58:43 -0400

    add most of the ticket tests

--- source/TicketTest.java
+++ source/TicketTest.java
@@ -6,27 +6,35 @@ class TicketTest {
 
 	@Test
 	void testTicket() {
-		fail("Not yet implemented");
+		Ticket ticket = new Ticket("Frozen", "123 theatre", 10.5);
+		assertEquals(new Ticket("Frozen", "123 theatre", 10.5),ticket);
 	}
 
 	@Test
 	void testGetShow() {
-		fail("Not yet implemented");
+		Ticket ticket = new Ticket("Frozen", "123 theatre", 10.5);
+		assertEquals("Frozen", ticket.getShow());
 	}
 
 	@Test
 	void testGetPrice() {
-		fail("Not yet implemented");
+		Ticket ticket = new Ticket("Frozen", "123 theatre", 10.5);
+		assertEquals(10.5, ticket.getPrice());
 	}
 
 	@Test
 	void testGetLocation() {
-		fail("Not yet implemented");
+		Ticket ticket = new Ticket("Frozen", "123 theatre", 10.5);
+		assertEquals("123 theatre", ticket.getLocation());
 	}
 
 	@Test
 	void testToString() {
-		fail("Not yet implemented");
+	String string =	" :: Thank you for your purchase to see " + "Frozen" + ":: \n"
+                + ":: The show you have paid to see will be at " +"123 theatre" + ". And finally you paid" + 10.5 + "for your ticket! :: \n"
+                + " :: Thanks again for purchasing at THE_247_PROJECT. Have a nice day and call (***)-***-**** if you have any issues!";
+	Ticket ticket = new Ticket("Frozen", "123 theatre", 10.5);
+	assertEquals(string, ticket.toString());
 	}
 
 	@Test
