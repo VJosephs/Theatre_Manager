dad0c719575375e0710dc0e35a55a554daec00bd
Andrew Merrill <realcoldfate@gmail.com>
2020-04-13 16:37:52 -0400

    started on SearcherTest

--- source/SearcherTest.java
+++ source/SearcherTest.java
@@ -1,12 +1,45 @@
-import static org.junit.jupiter.api.Assertions.*;
-
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
 class SearcherTest {
+	private ArrayList<Theatre> theatres;
+	private ArrayList<Show> shows;
+	private Searcher searcher;
+
+	@BeforeEach
+	void OneTimeSetup() {
+		Show show1 = new Show("frozen", "show", "horror", "R", new Date(2000, Calendar.JANUARY, 10), "123 theatre", 10);
+		Show show2 = new Show("X men", "show", "Action", "PG", new Date(2000, Calendar.JANUARY, 10), "123 theatre", 15);
+		String name = "name";
+		String address = "address";
+		Layout layout = new Layout("layout", new Seat[5][5]);
+		ArrayList<Rating> ratings = new ArrayList<>();
+		shows = new ArrayList<>();
+		shows.add(show1);
+		shows.add(show2);
+		Theatre t = new Theatre(name, address, layout, ratings, shows);
+		Theatre t2 = new Theatre(name+"2",address+"2",layout,ratings,shows);
+		theatres = new ArrayList<>();
+		theatres.add(t);
+		theatres.add(t2);
+
+
+		searcher = new Searcher(theatres, shows);
+	}
 
 	@Test
 	void testSearcher() {
-		fail("Not yet implemented");
+		searcher = new Searcher(theatres, shows);
+
+		assertEquals(theatres, searcher.getTheatres());
+		assertEquals(shows, searcher.getShows());
 	}
 
 	@Test
