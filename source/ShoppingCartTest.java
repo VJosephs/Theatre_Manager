9b9cbe574c0650c37f5e1daefb3c228df7a206af
jakobwb3 <jakobwb@email.sc.edu>
2020-04-13 16:59:55 -0400

    Added testVerifyAge and testGetCart

--- source/ShoppingCartTest.java
+++ source/ShoppingCartTest.java
@@ -45,17 +45,31 @@ class ShoppingCartTest {
 
 	@Test
 	void testPrintTickets() {
+		/*
+		ArrayList<Ticket> testList = new ArrayList<Ticket>();
+		ShoppingCart testCart = new ShoppingCart(testList);
+		Show show = new Show("Show", "String description", "String genre", "String ageRating", new Date(10,Calendar.JANUARY, 1), "String location",1);
+		Ticket ticket = new Ticket("Show", show.getLocation(), show.getPrice());
+		testCart.addTicket(ticket);
+		assertEquals(testCart.printTickets(), " :: Thank you for your purchase to see Show" + ":: \n"
+                + ":: The show you have paid to see will be at " + show.getLocation() + ". And finally you paid" + show.getPrice() + "for your ticket! :: \n"
+                + " :: Thanks again for purchasing at THE_247_PROJECT. Have a nice day and call (***)-***-**** if you have any issues!")
+        */
 		fail("Not yet implemented");
 	}
 
 	@Test
 	void testVerifyAge() {
-		fail("Not yet implemented");
+		ArrayList<Ticket> testList = new ArrayList<Ticket>();
+		ShoppingCart testCart = new ShoppingCart(testList);
+		assertEquals(testCart.verifyAge(), true);
 	}
 
 	@Test
 	void testGetCart() {
-		fail("Not yet implemented");
+		ArrayList<Ticket> testList = new ArrayList<Ticket>();
+		ShoppingCart testCart = new ShoppingCart(testList);
+		assertEquals(testCart.getCart(), testList);
 	}
 
 }
